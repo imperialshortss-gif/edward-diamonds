5e84e3756ec951b5e989215eb0aaf252424dadb0
imperialshortss-gif <imperialshortss@gmail.com>
2026-08-08 01:46:52 +0500

    Update Leadership.tsx

--- src/pages/Leadership.tsx
+++ src/pages/Leadership.tsx
@@ -1,4 +1,5 @@
 import { Layout } from "@/components/Layout";
+import { SEO } from "@/components/SEO";
 import chairmanImg from "@/assets/chairman.jpg";
 import ceoImg from "@/assets/ceo.jpg";
 import vpImg from "@/assets/vp.jpg";
