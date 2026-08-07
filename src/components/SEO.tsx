bd591810e2ff1ef74da09e49599b6e53091db75d
imperialshortss-gif <imperialshortss@gmail.com>
2026-08-08 01:41:15 +0500

    Create SEO.tsx

---
+++ src/components/SEO.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+interface SEOProps {
+  title: string;
+  description: string;
+}
+
+export function SEO({ title, description }: SEOProps) {
+  useEffect(() => {
+    document.title = title;
+
+    const metaDescription = document.querySelector(
+      'meta[name="description"]'
+    );
+
+    if (metaDescription) {
+      metaDescription.setAttribute("content", description);
+    }
+  }, [title, description]);
+
+  return null;
+}
